be8919f513adb82c2355da2e255df1bc30978f8b
Vivien Gajac <viviengajac@gmail.com>
2024-04-08 15:46:40 +0200

    ✅ update CharacterController

--- src/app/homepage-container/galaxy/character/character.component.ts
+++ src/app/homepage-container/galaxy/character/character.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataServiceService } from 'src/app/data-service.service';
+import { Data } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-character',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgFor],
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.scss']
 })
-export class CharacterComponent {
 
+export class CharacterComponent {
+  chars: any;
+  Math: Math = Math;
+  constructor(
+    private dataService: DataServiceService
+  )
+  {
+  }
+  
+  async ngOnInit():Promise<void>
+  {
+      this.chars = await this.dataService.getAllApprenant();
+      console.log(this.chars)
+  }
+  
 }
