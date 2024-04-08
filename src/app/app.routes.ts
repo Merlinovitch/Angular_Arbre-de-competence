05316b3af860029e533c990cc853c428e63a4d01
Vivien Gajac <viviengajac@gmail.com>
2024-04-08 09:48:20 +0200

    ✅ add GalaxyComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { GalaxyComponent } from './galaxy/galaxy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {component: GalaxyComponent, path:'/galaxy'}
+];
---
+++ src/app/galaxy/galaxy.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-galaxy',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './galaxy.component.html',
+  styleUrls: ['./galaxy.component.scss']
+})
+export class GalaxyComponent {
+
+}
