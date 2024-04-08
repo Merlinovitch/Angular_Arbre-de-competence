bc29e317a1ce4e576ae56201684f34a64ade86d4
Merlinovitch <a.merlin92@gmail.com>
2024-04-08 10:21:06 +0200

    feat: :art: Initialize Homepage Container component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { GalaxyComponent } from './galaxy/galaxy.component';
+import { HomepageContainerComponent } from './homepage-container/homepage-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, GalaxyComponent],
+  imports: [CommonModule, RouterOutlet, GalaxyComponent, HomepageContainerComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
