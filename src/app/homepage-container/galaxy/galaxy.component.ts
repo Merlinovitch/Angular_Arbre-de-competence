4cc35bcf33854ef9d43732656b2d0c15c244b262
Vivien Gajac <viviengajac@gmail.com>
2024-04-08 17:28:01 +0200

    ✅ update CharacterController

--- src/app/homepage-container/galaxy/galaxy.component.ts
+++ src/app/homepage-container/galaxy/galaxy.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CharacterComponent } from './character/character.component';
 
 @Component({
   selector: 'app-galaxy',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CharacterComponent],
   templateUrl: './galaxy.component.html',
   styleUrls: ['./galaxy.component.scss']
 })
