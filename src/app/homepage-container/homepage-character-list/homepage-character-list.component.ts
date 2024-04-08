7522f8dd0d49bb1b2de71fd22cc3512989873ec2
Merlinovitch <a.merlin92@gmail.com>
2024-04-08 11:35:38 +0200

    feat: :construction: add Footer, Homepage Character List and Initialize variables.scss

---
+++ src/app/homepage-container/homepage-character-list/homepage-character-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-homepage-character-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './homepage-character-list.component.html',
+  styleUrls: ['./homepage-character-list.component.scss']
+})
+export class HomepageCharacterListComponent {
+
+}
--- src/app/homepage-container/homepage-container.component.ts
+++ src/app/homepage-container/homepage-container.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalaxyComponent } from '../galaxy/galaxy.component';
+import { HomepageCharacterListComponent } from './homepage-character-list/homepage-character-list.component';
 
 @Component({
   selector: 'app-homepage-container',
   standalone: true,
-  imports: [CommonModule, GalaxyComponent],
+  imports: [CommonModule, GalaxyComponent, HomepageCharacterListComponent],
   templateUrl: './homepage-container.component.html',
   styleUrls: ['./homepage-container.component.scss']
 })
