2c657a9c649c9949382fd29c2e4abe83165ae795
Merlinovitch <a.merlin92@gmail.com>
2024-04-08 16:07:24 +0200

    add character list

--- src/app/homepage-container/homepage-character-list/homepage-character-list.component.ts
+++ src/app/homepage-container/homepage-character-list/homepage-character-list.component.ts
@@ -1,18 +1,23 @@
-import { Component, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { DataServiceService } from '../../data-service.service';
+import { CommonModule, NgFor } from '@angular/common';
+
 @Component({
   selector: 'app-homepage-character-list',
   standalone: true,
+  imports: [CommonModule, NgFor],
   templateUrl: './homepage-character-list.component.html',
   styleUrls: ['./homepage-character-list.component.scss']
 })
 
 
 export class HomepageCharacterListComponent {
+  chars: any;
+
   constructor(private dataService: DataServiceService) {}
 
-  async ngOnInit(): Promise<void> {
-
-}
+  async ngOnInit():Promise<void>
+  {
+      this.chars = await this.dataService.getAllApprenant();
+  }
 }
