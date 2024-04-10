1179d02a1e68d78d2b19e386eb42457c1572ebea
Vivien Gajac <viviengajac@gmail.com>
2024-04-10 10:40:55 +0200

    ✅ merge Galaxy & Character-list Components

--- src/app/homepage-container/homepage-character-list/homepage-character-list.component.ts
+++ src/app/homepage-container/homepage-character-list/homepage-character-list.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { DataServiceService } from '../../data-service.service';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-homepage-character-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgFor],
   templateUrl: './homepage-character-list.component.html',
   styleUrls: ['./homepage-character-list.component.scss']
 })
-export class HomepageCharacterListComponent {
 
+
+export class HomepageCharacterListComponent {
+  chars: any;
+
+  constructor(private dataService: DataServiceService) {}
+
+  async ngOnInit():Promise<void>
+  {
+      this.chars = await this.dataService.getAllApprenant();
+  }
 }
