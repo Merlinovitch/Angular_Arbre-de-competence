797f30e58aa1b7fc1b837c8df2d93567f433b9a3
Théo Duflos <theo.duflos.yokoko@gmail.com>
2024-04-10 12:09:46 +0200

    feat: :sparkles: add stat component

--- src/app/constellation-container/constellation-container.component.ts
+++ src/app/constellation-container/constellation-container.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PcConstellationComponent } from './pc-constellation/pc-constellation.component';
 import { LockConstellationComponent } from './lock-constellation/lock-constellation.component';
 import { FuseeConstellationComponent } from './fusee-constellation/fusee-constellation.component';
+import { StatComponent } from './stat/stat.component';
 
 @Component({
   selector: 'app-constellation-container',
   standalone: true,
-  imports: [CommonModule, PcConstellationComponent,LockConstellationComponent,FuseeConstellationComponent],
+  imports: [CommonModule, PcConstellationComponent,LockConstellationComponent,FuseeConstellationComponent,StatComponent],
   templateUrl: './constellation-container.component.html',
   styleUrls: ['./constellation-container.component.scss']
 })
