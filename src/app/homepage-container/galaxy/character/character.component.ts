036514ef1fb931c3218204d7490e2b015a8f62b6
Vivien Gajac <viviengajac@gmail.com>
2024-04-11 12:23:47 +0200

    ✅ add characters progress % displayed in galaxy

--- src/app/homepage-container/galaxy/character/character.component.ts
+++ src/app/homepage-container/galaxy/character/character.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, KeyValue } from '@angular/common';
 import { DataServiceService } from 'src/app/data-service.service';
 import { Data } from '@angular/router';
 import { NgFor } from '@angular/common';
@@ -24,22 +24,59 @@ export class CharacterComponent {
   
   async ngOnInit():Promise<void>
   {
-      this.chars = await this.dataService.getAllApprenant();
+    this.chars = await this.dataService.getAllApprenant();
+    
+    this.addLevelToCharacters();
+    
 
-      this.timers = [
-        "11",
-        "12.4",
-        "13.2",
-        "14",
-        "12.5",
-        "13.4",
-        "14.2",
-        "12.8",
-        "13.5",
-        "14.4",
-        "12.6",
-        "13.6",
-      ];
+    // this.getCharactersSkills();
+
+    this.timers = [
+      "11",
+      "12.4",
+      "13.2",
+      "14",
+      "12.5",
+      "13.4",
+      "14.2",
+      "12.8",
+      "13.5",
+      "14.4",
+      "12.6",
+      "13.6",
+    ];
+
   }
   
+  addLevelToCharacters()
+  {
+    this.chars.forEach((char: { [x: string]: string; }) => {
+      this.getCharacterSkillsById(char['id']).then(res => (char['lvl'] = `${res}`));
+    });
+  }
+  
+  getCharactersSkills():void
+  {
+    this.chars.forEach((character: any) => {
+      this.getCharacterSkillsById(character.id);
+    });
+  }
+
+  async getCharacterSkillsById(characterID: string):Promise<number>
+  {
+    const calcLvl = await this.dataService.getCompentenceByIdApprenant(characterID).then(res => this.calcCharacterGlobalLevel(res));
+    return calcLvl;
+  }
+
+  calcCharacterGlobalLevel(skills: any):number
+  {
+    let currentLevel = 0;
+
+    skills.forEach((skill: { niveau: number; }) => {
+      currentLevel += skill.niveau;
+    });
+    
+    const globalLevel = 100 / (11 * 3) * currentLevel;
+    return globalLevel;
+  }
 }
