a9d6fca9d95225d978022a641b67faebe458b093
yokokoricot <theo.duflos.yokoko@gmail.com>
2024-04-11 15:28:40 +0200

    feat: :sprakles: Add routing and some new desing on pc constellation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { GalaxyComponent } from './homepage-container/galaxy/galaxy.component';
 import { ConstellationContainerComponent } from './constellation-container/constellation-container.component';
+import { HomepageContainerComponent } from './homepage-container/homepage-container.component';
 
 export const routes: Routes = [
-  { component: GalaxyComponent, path: 'galaxy' },
-  { component: ConstellationContainerComponent, path: 'constellation'},
+  { component:HomepageContainerComponent, path:''},
+  { component: ConstellationContainerComponent, path: 'constellation/:id'},
 ];
--- src/app/homepage-container/homepage-container.component.ts
+++ src/app/homepage-container/homepage-container.component.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { GalaxyComponent } from '../homepage-container/galaxy/galaxy.component';
 import { CharacterComponent } from '../homepage-container/galaxy/character/character.component';
 import { HomepageCharacterListComponent } from './homepage-character-list/homepage-character-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { routes } from '../app.routes';
 
 
 @Component({
   selector: 'app-homepage-container',
   standalone: true,
-  imports: [CommonModule, GalaxyComponent, CharacterComponent, HomepageCharacterListComponent],
+  imports: [CommonModule, GalaxyComponent, CharacterComponent, HomepageCharacterListComponent, RouterModule],
   templateUrl: './homepage-container.component.html',
   styleUrls: ['./homepage-container.component.scss']
 })
