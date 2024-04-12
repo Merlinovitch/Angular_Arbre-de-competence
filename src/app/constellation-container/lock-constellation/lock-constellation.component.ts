0743399d6df2882eada50e8ba52151662d9db1d5
yokokoricot <theo.duflos.yokoko@gmail.com>
2024-04-12 17:18:22 +0200

    fix: :bug: Fix bug where data who's display only in the first component, move star-info in parents container

--- src/app/constellation-container/lock-constellation/lock-constellation.component.ts
+++ src/app/constellation-container/lock-constellation/lock-constellation.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataServiceService } from 'src/app/data-service.service';
+import { ActivatedRoute } from '@angular/router';
+import { StarInfoComponent } from '../star-info/star-info.component';
 
 @Component({
   selector: 'app-lock-constellation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, StarInfoComponent],
   templateUrl: './lock-constellation.component.html',
   styleUrls: ['./lock-constellation.component.scss']
 })
 export class LockConstellationComponent {
+  idApprenant: string = '';
+  @Output()
+  public idStarHover: EventEmitter<{string: string,event :Event}> = new EventEmitter<{string: string,event :Event}>();
+  @Output()
+  public starLeave: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  constructor() {
+  }
+
+  showStar(idApprenant: string,event: Event): void {
+    this.idStarHover.emit({string: idApprenant,event: event});
+  }
+
+  hideStar() {
+    this.starLeave.emit(true);
+  }
+
+  
 }
--- src/app/constellation-container/pc-constellation/pc-constellation.component.ts
+++ src/app/constellation-container/pc-constellation/pc-constellation.component.ts
@@ -1,8 +1,9 @@
-import { Component, ElementRef, Input, Renderer2 } from '@angular/core';
+import { Component, ComponentFactoryResolver, ElementRef, EventEmitter, Input, Output, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StarInfoComponent } from '../star-info/star-info.component';
 import { DataServiceService } from 'src/app/data-service.service';
 import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-pc-constellation',
@@ -12,57 +13,21 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./pc-constellation.component.scss'],
 })
 export class PcConstellationComponent {
-  tooltipContent = {
-    Competence : {
-      title: ""
-    }
-  };
-  data:any;
+
   idApprenant: string = '';
-  constructor(
-    private dataService: DataServiceService,
-    private renderer: Renderer2,
-    private route: ActivatedRoute
-  ) {
-    this.route.params.subscribe( parms=> this.idApprenant = parms["id"]);
+  @Output()
+  public idStarHover: EventEmitter<{string: string,event :Event}> = new EventEmitter<{string: string,event :Event}>();
+  @Output()
+  public starLeave: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  constructor() {
   }
 
-  async ngOnInit(): Promise<void> {
-    this.data="";
-    try {
-      this.data = await this.dataService.getCompentenceByIdApprenant(this.idApprenant);
-    } catch (e) {
-      console.error('error', e);
-    }
-    console.log(this.data);
-    
+  showStar(idApprenant: string,event: Event): void {
+    this.idStarHover.emit({string: idApprenant,event: event});
   }
 
-  async showStar(event: MouseEvent, elementId: string): Promise<any> {
-
-    
-    const tooltip = document.querySelector('app-star-info');
-    if (tooltip) {
-      this.renderer.setStyle(tooltip, 'display', 'block');
-      this.renderer.setStyle(tooltip, 'position', 'absolute');
-      this.renderer.setStyle(tooltip, 'left', event.clientX + 20 + 'px');
-      this.renderer.setStyle(tooltip, 'top', event.clientY + 'px');
-      console.log(elementId);
-      
-      const row = this.data.find((e :any) => e.Competence.id == elementId)
-      console.log(row.niveau);
-      
-      if (row) {
-        this.tooltipContent = row
-      }
+  hideStar() {
+    this.starLeave.emit(true);
   }
-}
-  hideStar(event: MouseEvent) {
-    const tooltip = document.querySelector('app-star-info');
-    if (tooltip) {
-      this.renderer.setStyle(tooltip, 'display', 'none');
-      console.log('goodbye');
-    }
-  }
-
 }
