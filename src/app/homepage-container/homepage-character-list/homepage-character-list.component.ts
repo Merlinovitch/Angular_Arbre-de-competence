0ec60b451358b760046499919e749903d83862ba
Vivien Gajac <viviengajac@gmail.com>
2024-04-12 09:08:49 +0200

    ✅ add loader and page 2 style

--- src/app/homepage-container/homepage-character-list/homepage-character-list.component.ts
+++ src/app/homepage-container/homepage-character-list/homepage-character-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 import { DataServiceService } from '../../data-service.service';
 import { CommonModule, NgFor } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-homepage-character-list',
@@ -16,7 +16,7 @@ export class HomepageCharacterListComponent {
   chars: any;
   hoveredCharID: any;
 
-  constructor(private dataService: DataServiceService) {}
+  constructor(private dataService: DataServiceService, private router: Router) {}
 
   async ngOnInit():Promise<void>
   {
@@ -55,6 +55,59 @@ export class HomepageCharacterListComponent {
           {
             this.removeFocus();
           }
+      break;
+    }
+  }
+
+  @HostListener('window:click', ['$event']) onMouseClick(e: any)
+  {
+    switch (e.target.className)
+    {
+      case "homepage-character-card-name":
+      case "character-content":
+
+        let characterID: string = '';
+
+        if (e.target.className === "homepage-character-card-name")
+          {
+            characterID = e.target.id.substring(15);
+          } else if (e.target.className === "character-content") {
+            characterID = e.target.id.substring(10);
+          }
+        
+        const target: any = document.querySelector(`#character-${characterID}`);
+        const position = target.getBoundingClientRect();
+        const x = Math.floor(position.top);
+        const y = Math.floor(position.left);
+
+        const galaxy: any = document.querySelector(".galaxy");
+        galaxy.style.transform = `scale(4) translateZ(-100px)`;
+        galaxy.style.transformOrigin = `${y / 2}px ${x / 2}px`;
+
+        const characters: any = document.querySelectorAll(".character-content");
+
+        characters.forEach((char: { id: string; style: { opacity: number; }; }) => {
+          console.log(char)
+          if (char !== target)
+            {
+              char.style.opacity = 0;
+            }
+        });
+        
+        const loadingScreen: any = document.querySelector(".homepage-loader");
+        loadingScreen.classList.toggle("loader-visible");
+        setTimeout(() => {
+          loadingScreen.classList.toggle("loader-visible");
+        }, 2000);
+
+        setTimeout(() => {
+          this.router.navigate([`/constellation/${characterID}`]);
+        }, 2000);
+
+      break;
+
+      default:
+
       break;
     }
   }
---
+++ src/assets/js/loader.js
@@ -0,0 +1,13 @@
+// setTimeout(() => {
+//   document.querySelector(".homepage-loader").remove();
+// }, 5000);
+
+// document.onreadystatechange = function() {
+//   if (document.readyState !== "complete") {
+//       document.querySelector("body").style.visibility = "hidden";
+//       document.querySelector(".homepage-loader").style.visibility = "visible";
+//   } else {
+//       document.querySelector(".homepage-loader").style.display = "none";
+//       document.querySelector("body").style.visibility = "visible";
+//   }
+// };
