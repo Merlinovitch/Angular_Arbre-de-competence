0cf40a1e42193275ab7c84d3201be38270cc0e71
yokokoricot <theo.duflos.yokoko@gmail.com>
2024-04-13 23:40:34 +0200

    feat: :sparkles: change star colors with level and add animation to it

--- src/app/constellation-container/pc-constellation/pc-constellation.component.ts
+++ src/app/constellation-container/pc-constellation/pc-constellation.component.ts
@@ -4,6 +4,7 @@ import { StarInfoComponent } from '../star-info/star-info.component';
 import { DataServiceService } from 'src/app/data-service.service';
 import { ActivatedRoute } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
+import { trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-pc-constellation',
@@ -16,12 +17,44 @@ export class PcConstellationComponent {
 
   idApprenant: string = '';
   @Input() activityTitle: string | undefined;
+  @Input() data:any
   @Output()
   public idStarHover: EventEmitter<{string: string,event :Event}> = new EventEmitter<{string: string,event :Event}>();
   @Output()
   public starLeave: EventEmitter<boolean> = new EventEmitter<boolean>();
-
+  star34: number = 0;
+  star35: number = 0;
+  star36: number = 0;
+  star37: number = 0;
   constructor() {
+    
+  }
+
+  ngAfterViewInit(): void {
+    console.log('in');
+    
+    this.data.forEach((element:any) => {  
+      switch(element.Competence.id){        
+        case 34: {
+          this.star34 = element.niveau
+          break;
+        }
+        case 35: {
+          this.star35 = element.niveau
+          break;
+        }
+        case 36: {
+          this.star36 = element.niveau
+          break;
+        }
+        case 37: {
+          this.star37 = element.niveau
+          break;
+        }
+        default:
+          break;
+      }
+    });
   }
 
   showStar(idApprenant: string,event: Event): void {
